968cca0fc288b21fbbc81e906b05a4420aa44b76
kamite0611 <kamiteakira@gmail.com>
2025-01-05 20:36:44 +0900

    save

--- api/core/settings.py
+++ api/core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'src.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
