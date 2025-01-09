a02f2cf9431ce9a5324ab21ae663287316004be3
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-09 12:04:22 +0900

    init model

--- api/pong/models.py
+++ api/pong/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    display_name = models.CharField(max_length=50, unique=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True, default="default_avatar.png")
+    online_status = models.BooleanField(default=False)
+    friends = models.ManyToManyField("self", symmetrical=True, blank=True)
+
+    def __str__(self):
+        return self.display_name
+    
+class Game(models.Model):
+    player1 = models.ForeignKey(User, related_name="games_as_player1", on_delete=models.CASCADE)
+    player2 = models.ForeignKey(User, related_name="games_as_player2", on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    winner = models.ForeignKey(User, related_name="games_won", null=True, blank=True, on_delete=models.SET_NULL)
+    score_player1 = models.IntegerField(default=0)
+    score_player2 = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Game {self.id} - {self.player1.display_name} vs {self.player2.display_name}"
