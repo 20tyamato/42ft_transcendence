9fd9899a49af694db069d08c3b74eae9368d44d6
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-10 12:49:11 +0900

    Added Admin

--- api/pong/admin.py
+++ api/pong/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Game
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "display_name",
+        "email",
+        "level",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    search_fields = ("username", "display_name", "email")
+    list_filter = ("is_staff", "is_superuser", "is_active", "level")
+
+    fieldsets = (
+        (None, {
+            "fields": ("username", "password")
+        }),
+        ("Personal info", {
+            "fields": ("display_name", "avatar", "email", "level")
+        }),
+        ("Permissions", {
+            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+        }),
+        ("Important dates", {
+            "fields": ("last_login", "date_joined")
+        }),
+    )
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "player1",
+        "player2",
+        "start_time",
+        "end_time",
+        "winner",
+        "score_player1",
+        "score_player2",
+        "is_ai_opponent",
+    )
+    list_filter = ("is_ai_opponent", "winner", "start_time")
+    search_fields = ("player1__display_name", "player2__display_name")
+
+admin.site.register(User, CustomUserAdmin)
