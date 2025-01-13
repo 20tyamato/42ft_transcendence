41e87a834d2099c9c044e64205389789f2a72770
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-13 22:22:19 +0900

    FIX

--- frontend/src/pages/Home/index.ts
+++ frontend/src/pages/Home/index.ts
@@ -1,8 +1,8 @@
 import { Page } from '@/core/Page';
 import CommonLayout from '@/layouts/common/index';
 
-const HomePage = new Page({
-  name: 'Home',
+const OpeningPage = new Page({
+  name: 'Opening',
   config: {
     layout: CommonLayout,
   },
@@ -10,32 +10,28 @@ const HomePage = new Page({
     const quickStartBtn = document.querySelector('a[href="/quickstart"]');
     const singlePlayBtn = document.querySelector('a[href="/singleplay"]');
     const multiPlayBtn = document.querySelector('a[href="/multiplay"]');
-    const registerButton = document.querySelector('a[href="/register"]');
 
     quickStartBtn?.addEventListener('click', (event) => {
       event.preventDefault();
       alert('Quick Start clicked!');
+      // 画面遷移
       window.location.href = '/quickstart';
     });
 
     singlePlayBtn?.addEventListener('click', (event) => {
       event.preventDefault();
       alert('Single Play clicked!');
+      // 画面遷移
       window.location.href = '/singleplay';
     });
 
     multiPlayBtn?.addEventListener('click', (event) => {
       event.preventDefault();
       alert('Multi Play clicked!');
+      // 画面遷移
       window.location.href = '/multiplay';
-    });
-
-    registerButton?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Register button clicked! Navigating to the register page...');
-      window.location.href = '/register';
     });
   },
 });
 
-export default HomePage;
+export default OpeningPage;
