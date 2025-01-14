0ecce35fd436a69a80c852c271041f619312fd32
kfukuhar <frics5090fksk@gmail.com>
2025-01-14 17:35:57 +0900

    wip #29

--- api/pong/serializers.py
+++ api/pong/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import User, Game, Tournament, BlockchainScore
+from django.contrib.auth.password_validation import validate_password
 
 # TODO: ここ追加する
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    
     class Meta:
         model = User
         fields = [
@@ -10,8 +14,36 @@ class UserSerializer(serializers.ModelSerializer):
             'username',
             'email',
             'display_name',
+            'password',
+            'password2'
         ]
+    def validate(self, attrs):
+        if attes['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        
+        try:
+            validate_password(attrs['password'])
+        except ValidationError as e:
+            raise serializers.ValidationError({"password": list(e.messages)})
+        
+        return attrs
 
+    def create(self, validated_data):
+        # Remove password2 as we don't need it for user creation
+        validated_data.pop('password2', None)
+
+        # Create user instance but don't save it yet
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            display_name=validate_data['display_name']
+        )
+
+        # Set password (this handles the hashing)
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
 
 class GameSerializer(serializers.ModelSerializer):
     player1 = UserSerializer(read_only=True)
