04c806aa2662fef250f1aa98a72773fcfc908cdb
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-01-17 10:12:07 +0900

    Sort

--- frontend/src/pages/Home/index.ts
+++ frontend/src/pages/Home/index.ts
@@ -7,54 +7,11 @@ const HomePage = new Page({
     layout: CommonLayout,
   },
   mounted: async () => {
-    const quickStartBtn = document.querySelector('a[href="/quickstart"]');
-    const singlePlayBtn = document.querySelector('a[href="/singleplay"]');
-    const multiPlayBtn = document.querySelector('a[href="/multiplay"]');
-    const registerBtn = document.querySelector('a[href="/register"]');
     const loginBtn = document.querySelector('a[href="/login"]');
-    const rankingBtn = document.querySelector('a[href="/ranking"]');
-    const profileBtn = document.querySelector('a[href="/userprofile"]');
-
-    quickStartBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Quick Start clicked!');
-      window.location.href = '/quickstart';
-    });
-
-    singlePlayBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Single Play clicked!');
-      window.location.href = '/singleplay';
-    });
-
-    multiPlayBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Multi Play clicked!');
-      window.location.href = '/multiplay';
-    });
-
-    registerBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Register clicked!');
-      window.location.href = '/register';
-    });
 
     loginBtn?.addEventListener('click', (event) => {
       event.preventDefault();
-      alert('Login clicked!');
       window.location.href = '/login';
-    });
-
-    rankingBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('Ranking clicked!');
-      window.location.href = '/ranking';
-    });
-
-    profileBtn?.addEventListener('click', (event) => {
-      event.preventDefault();
-      alert('User Profile clicked!');
-      window.location.href = '/userprofile';
     });
   },
 });
