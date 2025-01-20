5db4b976ca0076992d84f1d294dffd8e12f05649
tatashir <115047593+tatashir@users.noreply.github.com>
2025-01-20 10:12:04 +0900

    Usernameを取得

--- frontend/src/pages/Profile/index.ts
+++ frontend/src/pages/Profile/index.ts
@@ -17,14 +17,16 @@ const ProfilePage = new Page({
     layout: CommonLayout,
   },
   mounted: async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/users/');
+    const data = await response.json();
+    const userData = data[0];
     const usernameEl = document.getElementById('username') as HTMLElement;
     const rankingEl = document.getElementById('ranking') as HTMLElement;
     const tournamentHistoryEl = document.getElementById('tournamentHistory');
     const scoreListEl = document.getElementById('scoreList');
     const editBtn = document.getElementById('editBtn');
 
-    // 本来はAPIやDBから取得する
-    const username = 'JohnDoe42';
+    const username = userData.username;
     const ranking = 5;
     const tournamentHistory: ITournamentHistory[] = [
       { date: '2025-01-01', result: 'Won' },
