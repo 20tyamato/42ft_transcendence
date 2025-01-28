67458306c937731c035e3c9543c602b991655256
KENTA FUKUHARA <33676717+FRISKSYA@users.noreply.github.com>
2025-01-28 22:21:25 +0900

    REPLACE: from runserver to daphne (#77)
    * done replace_daphne #76
    
    * done ruff
    
    * add redis #76 #78
    
    * add FIXME

--- api/core/settings.py
+++ api/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -73,8 +74,19 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+# Channels
+# WSGI_APPLICATION = 'core.wsgi.application'
+ASGI_APPLICATION = 'core.asgi.application'
 
+# Channel Layers
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
