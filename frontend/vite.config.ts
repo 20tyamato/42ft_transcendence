71646f91ba64ea93e1d770c5712a23760f9ff78b
kamite0611 <kamiteakira@gmail.com>
2025-02-05 22:48:25 +0900

    fix: ホットリロードされるようにしました

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,18 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+      port: 24678,
+      clientPort: 24678,
+      host: '0.0.0.0',
+    },
     port: 3001,
     host: '0.0.0.0',
   },
