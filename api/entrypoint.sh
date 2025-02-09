a21dab03c230ece5845ed1b7e597944037e36352
kamite0611 <57242907+kamite0611@users.noreply.github.com>
2025-02-09 18:38:53 +0900

    Djangoホットリロードできるように (#115)
    * fix: Djangoホットリロードできるように修正
    
    * delete apiの不要な環境変数削除
    
    * daphne を使用した状態でホットリロードされるように
    
    * delete 不要な記述

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -6,5 +6,13 @@ python manage.py migrate
 # 初期データの読み込み
 python manage.py loaddata initial_data
 
-# サーバーの起動
-exec poetry run daphne -b 0.0.0.0 -p 8000 core.asgi:application
+
+# 開発環境用のホットリロード設定
+if [ "$DJANGO_ENV" = "development" ]; then
+    # watchdogを使用してファイル変更を監視
+    watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- \
+    poetry run daphne -b 0.0.0.0 -p 8000 core.asgi:application
+else
+    # 本番環境ではdaphneのみ起動
+    exec poetry run daphne -b 0.0.0.0 -p 8000 core.asgi:application
+fi
