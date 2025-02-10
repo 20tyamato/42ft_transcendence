6e7f886cf24db137247328122f0e4a4a4fdf12a7
[TYPrivate] <[tsukasa.soccer8@gmail.com]>
2025-02-10 19:32:48 +0900

    Fixing Avatar upload

--- frontend/src/pages/Modes/index.ts
+++ frontend/src/pages/Modes/index.ts
@@ -1,5 +1,24 @@
+import { API_URL } from '@/config/config';
 import { Page } from '@/core/Page';
 import LoggedInLayout from '@/layouts/loggedin/index';
+
+const token = localStorage.getItem('token');
+const username = localStorage.getItem('username');
+
+const fetchCurrentUser = async () => {
+  const response = await fetch(`${API_URL}/api/users/me/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Token ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch user data');
+  }
+  return response.json();
+};
 
 const ModesPage = new Page({
   name: 'Modes',
@@ -7,6 +26,13 @@ const ModesPage = new Page({
     layout: LoggedInLayout,
   },
   mounted: async () => {
+    const avatarEl = document.getElementById('avatar') as HTMLImageElement;
+
+    const userData = await fetchCurrentUser();
+    if (avatarEl) {
+      avatarEl.src = userData.avatar || '/src/layouts/common/avatar.png';
+    }
+
     const singleModeButton = document.querySelector('.single-mode');
     const multiModeButton = document.querySelector('.multi-mode');
     const tournamentModeButton = document.querySelector('.tournament-mode');
