e6abd4f5fd1c290a67be3d1d6b7f36b2c792bf7e
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-02-15 22:13:19 +0900

    add models for tournament #156

--- api/pong/tests/test_model.py
+++ api/pong/tests/test_model.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
 from pong.models import Game, User
 
@@ -125,3 +126,100 @@ class GameModelTests(TestCase):
         human_game.winner = self.player2
         human_game.save()
         self.assertEqual(human_game.winner, self.player2)
+
+
+class TournamentModelTests(TestCase):
+    def setUp(self):
+        """
+        Create some users and games to be used in the Tournament tests.
+        """
+        self.user1 = User.objects.create_user(
+            username="tournament_user1",
+            password="tournament_pass1",
+            display_name="Tournament User 1",
+        )
+        self.user2 = User.objects.create_user(
+            username="tournament_user2",
+            password="tournament_pass2",
+            display_name="Tournament User 2",
+        )
+
+        self.game1 = Game.objects.create(
+            player1=self.user1, player2=self.user2, score_player1=12, score_player2=10
+        )
+        self.game2 = Game.objects.create(
+            player1=self.user2, player2=self.user1, score_player1=5, score_player2=7
+        )
+
+    def test_create_tournament(self):
+        """
+        Test that a Tournament can be created successfully.
+        """
+        tournament = Tournament.objects.create(name="Test Tournament")
+        # M2Mフィールドを追加
+        tournament.participants.set([self.user1, self.user2])
+        tournament.games.set([self.game1, self.game2])
+        tournament.save()
+
+        self.assertIsNotNone(
+            tournament.id,
+            "Tournament should be created successfully and assigned an ID",
+        )
+        self.assertEqual(tournament.name, "Test Tournament")
+        self.assertIn(self.user1, tournament.participants.all())
+        self.assertIn(self.user2, tournament.participants.all())
+        self.assertIn(self.game1, tournament.games.all())
+        self.assertIn(self.game2, tournament.games.all())
+
+    def test_str_representation(self):
+        """
+        Test that the __str__ method returns the name of the Tournament.
+        """
+        tournament = Tournament.objects.create(name="Friendly Cup")
+        self.assertEqual(str(tournament), "Friendly Cup")
+
+    def test_blockchain_score_hash(self):
+        """
+        Test that we can store a blockchain_score_hash for the tournament.
+        """
+        tournament = Tournament.objects.create(
+            name="Hash Test Tournament", blockchain_score_hash="abc123xyz"
+        )
+        self.assertEqual(tournament.blockchain_score_hash, "abc123xyz")
+
+
+class BlockchainScoreModelTests(TestCase):
+    def setUp(self):
+        """
+        Create a tournament to be used for BlockchainScore tests.
+        """
+        self.tournament = Tournament.objects.create(name="Blockchain Tournament")
+
+    def test_create_blockchain_score(self):
+        """
+        Test that a BlockchainScore can be created successfully.
+        """
+        bc_score = BlockchainScore.objects.create(
+            tournament=self.tournament,
+            transaction_id="tx_12345",
+            blockchain_address="0xABCDEF",
+        )
+        self.assertIsNotNone(
+            bc_score.id,
+            "BlockchainScore should be created successfully and assigned an ID",
+        )
+        self.assertEqual(bc_score.tournament, self.tournament)
+        self.assertEqual(bc_score.transaction_id, "tx_12345")
+        self.assertEqual(bc_score.blockchain_address, "0xABCDEF")
+
+    def test_str_representation(self):
+        """
+        Test that the __str__ method returns the correct string format.
+        """
+        bc_score = BlockchainScore.objects.create(
+            tournament=self.tournament,
+            transaction_id="tx_67890",
+            blockchain_address="0x123456",
+        )
+        expected_str = f"Blockchain Score for Tournament {self.tournament.name}"
+        self.assertEqual(str(bc_score), expected_str)
