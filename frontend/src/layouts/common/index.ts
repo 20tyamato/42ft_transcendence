1e334d0ad32a6435afbff26a125401ae1df8f30c
Rt3mis10 <117369494+hrinka@users.noreply.github.com>
2025-02-17 03:00:54 +0900

    FIX: add <div#screen-error/> in index.html

--- frontend/src/layouts/common/index.ts
+++ frontend/src/layouts/common/index.ts
@@ -1,3 +1,46 @@
+// import { Layout } from '@/core/Layout';
+// import logoPic from './42_logo.svg';
+// import avatarPic from './avatar.png';
+// import logout from './logout.png';
+
+// const CommonLayout = new Layout({
+//   name: 'common',
+// });
+
+// // TODO: change to env
+// const MIN_WIDTH = 1024;
+// const MIN_HEIGHT = 768;
+// const DESIRED_RATIO = 16 / 9;
+// const RATIO_TOLERANCE = 0.1;
+
+// function checkScreenSize() {
+
+//   const errorOverlay = document.getElementById('screen-error');
+//   if (!errorOverlay) {
+//     console.error('Error: The #screen-error element for displaying errors does not exist.');
+//     return;
+//   }
+
+//   const currentWidth = window.innerWidth;
+//   const currentHeight = window.innerHeight;
+//   const currentRatio = currentWidth / currentHeight;
+//   if (currentWidth < MIN_WIDTH || currentHeight < MIN_HEIGHT) {
+//     errorOverlay.classList.add('active');
+//     return;
+//   }
+//   const ratioDifference = Math.abs(currentRatio - DESIRED_RATIO) / DESIRED_RATIO;
+//   if (ratioDifference > RATIO_TOLERANCE) {
+//     errorOverlay.classList.add('active');
+//     return;
+//   }
+//   errorOverlay.classList.remove('active');
+// }
+
+// window.addEventListener('load', checkScreenSize);
+// window.addEventListener('resize', checkScreenSize);
+
+// export default CommonLayout;
+// export { avatarPic, checkScreenSize, logoPic, logout };
 import { Layout } from '@/core/Layout';
 import logoPic from '@/resources/42_logo.svg';
 import avatarPic from '@/resources/avatar.png';
--- frontend/src/pages/SinglePlay/Utils/sources.ts
+++ frontend/src/pages/SinglePlay/Utils/sources.ts
@@ -8,4 +8,5 @@ const sources: Source[] = [
   { name: 'scene', type: 'gltfModel', path: '/models/scene.glb' },
   { name: 'texture1', type: 'texture', path: '/textures/texture1.jpg' },
 ];
+
 export default sources;
