f58c0aa545e0bc988bb3ba2fa00a43840eddc185
Rt3mis10 <117369494+hrinka@users.noreply.github.com>
2025-02-17 14:17:25 +0900

    loaded glb successfully

--- frontend/src/pages/SinglePlay/Utils/Loaders.ts
+++ frontend/src/pages/SinglePlay/Utils/Loaders.ts
@@ -2,12 +2,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import EventEmitter from './EventEmitter';
 import sources, { Source } from './sources';
 
-// interface Source {
-//   name: string;
-//   type: 'gltfModel';
-//   path: string;
-// }
-
 export default class Loaders extends EventEmitter {
   private sources: Source[];
   private items: { [key: string]: any } = {};
@@ -57,27 +51,6 @@ export default class Loaders extends EventEmitter {
       }
     }
   }
-
-  // private startLoading(): void {
-  //   for (const source of this.sources) {
-  //     if (source.type === 'gltfModel') {
-  //       // this.loaders.gltfLoader.load('/models/scene.gltf', (file) =>
-  //       //   this.sourceLoaded(source, file)
-  //       // );
-  //     }
-  //     this.loaders.gltfLoader.load(
-  //       '/models/scene.glb',
-  //       (gltf) => {
-  //         console.log('GLTFモデルがロードされました:', gltf);
-  //         this.sourceLoaded(source, gltf);
-  //       },
-  //       undefined,
-  //       (error) => {
-  //         console.error('GLTFモデルのロードエラー:', error);
-  //       }
-  //     );
-  //   }
-  // }
 
   private sourceLoaded(source: Source, file: any): void {
     console.log(`Loaded: ${source.name}`, file);
