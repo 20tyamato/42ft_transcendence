de966a637e29ad748fcc15f436bb619a70a4be40
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-02-21 21:37:45 +0900

    init tournament index #157

--- frontend/src/pages/Tournament/index.ts
+++ frontend/src/pages/Tournament/index.ts
@@ -1,19 +1,21 @@
+// frontend/src/pages/Tournament/index.ts
 import { Page } from '@/core/Page';
 import CommonLayout from '@/layouts/common/index';
-import { checkUserAccess } from '@/models/User/auth';
+import './style.css';
 
 const TournamentPage = new Page({
   name: 'Tournament',
   config: {
     layout: CommonLayout,
+    html: '/src/pages/Tournament/index.html',
   },
-  mounted: async () => {
-    checkUserAccess();
-    const backBtn = document.getElementById('backBtn');
-
-    backBtn?.addEventListener('click', () => {
-      window.location.href = '/';
-    });
+  mounted: () => {
+    const startTournament = document.getElementById('start-tournament');
+    if (startTournament) {
+      startTournament.addEventListener('click', () => {
+        window.location.href = '/tournament/waiting';
+      });
+    }
   },
 });
 
