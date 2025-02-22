82a1f2b17745b3a62cdc90fa568190743ad46809
20tyamato <106132296+20tyamato@users.noreply.github.com>
2025-02-22 19:05:51 +0900

    FIX: リファクタリング PART 5 #135 (#195)
    * WIP
    
    * WIP
    
    * auto logout done
    
    * refractoring
    
    * refractoring
    
    * refractoring
    
    * refractoring
    
    * refractoring
    
    * refractoring
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Added new members
    
    * bug
    
    * bug
    
    * bug
    
    * give up
    
    * minor change
    
    * minor change
    
    ---------
    
    Co-authored-by: [TYPrivate] <[tsukasa.soccer8@gmail.com]>

--- frontend/src/models/User/auth.ts
+++ frontend/src/models/User/auth.ts
@@ -1,12 +1,48 @@
+import { API_URL } from '@/config/config';
+import { updateOnlineStatus } from '@/models/User/repository';
+
 export function checkUserAccess(): string {
   const token = localStorage.getItem('token');
   if (!token) {
     window.location.href = '/login';
     throw new Error('Token does not exist, redirected to login page.');
   }
+  updateOnlineStatus(true);
   return token;
 }
 
 export function isLoggedIn(): boolean {
   return !!localStorage.getItem('token');
 }
+
+export const executeLogout = async (): Promise<void> => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    console.warn('No token found in local storage. Skipping logout.');
+    return;
+  }
+  await updateOnlineStatus(false);
+  console.log('token:', token);
+
+  const url = `${API_URL}/api/logout/`;
+  const data = JSON.stringify({});
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      localStorage.clear();
+    } else {
+      console.error('Logout API call failed with status:', response.status);
+    }
+  } catch (error) {
+    console.error('Logout API call failed:', error);
+  }
+};
--- frontend/src/models/Window/repository.ts
+++ frontend/src/models/Window/repository.ts
@@ -1,44 +1,19 @@
-import { API_URL } from '@/config/config';
-import { updateOnlineStatus } from '@/models/User/repository';
-
 const IDLE_TIMEOUT = 10000; // 10秒
-
-const clearUserSession = (): void => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('username');
-  localStorage.clear();
-};
-
-// idleTimer をモジュールスコープで保持
 let idleTimer: number | null = null;
 
 export const resetTimer = () => {
-  const idleTimeout = IDLE_TIMEOUT;
-
   if (idleTimer !== null) {
     clearTimeout(idleTimer);
   }
 
   idleTimer = window.setTimeout(() => {
-    try {
-      updateOnlineStatus(false);
-      fetch(`${API_URL}/api/logout/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-      });
-      clearUserSession();
-      window.location.href = '/login';
-    } catch (error) {
-      console.error('Logout API call failed:', error);
-    }
-  }, idleTimeout);
+    window.location.href = '/logout';
+    idleTimer = null;
+  }, IDLE_TIMEOUT);
 };
 
 export const initResetTimerListeners = (): void => {
-  const events = ['mousemove', 'keydown', 'click', 'scroll'];
+  const events = ['mousemove', 'keydown', 'click', 'scroll', 'touchstart', 'resize'];
   events.forEach((event) => {
     window.addEventListener(event, resetTimer);
   });
