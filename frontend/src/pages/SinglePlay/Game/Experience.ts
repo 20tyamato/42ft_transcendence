1a13ff4fea99d950e98bd0107fb2b2f6afd48628
hrinka <hrinka@student.42tokyo.jp>
2025-02-25 14:59:51 +0900

    rebase

--- frontend/src/pages/SinglePlay/Game/Experience.ts
+++ frontend/src/pages/SinglePlay/Game/Experience.ts
@@ -56,6 +56,10 @@ export default class Experience {
   private localGameStarted: boolean = false;
 
   private constructor(canvas: HTMLCanvasElement) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+
     Experience.instance = this;
     window.experience = this;
     window.incorrectDevice = false;
