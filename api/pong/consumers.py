cb0a004d9bd683674ca22ec27e03840d138e6cc4
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-03-14 15:24:11 +0900

    done #245

--- api/pong/consumers.py
+++ api/pong/consumers.py
@@ -10,33 +10,6 @@ from .game_logic import MultiplayerPongGame
 from .models import Game, User
 
 
-class TestConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.channel_layer.group_add("test_group", self.channel_name)
-        await self.accept()
-        print("WebSocket connected!")
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("test_group", self.channel_name)
-        print(f"WebSocket disconnected with code: {close_code}")
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        print(f"Received message: {message}")
-
-        # グループにメッセージをブロードキャスト
-        await self.channel_layer.group_send(
-            "test_group", {"type": "chat_message", "message": message}
-        )
-
-    async def chat_message(self, event):
-        message = event["message"]
-
-        # WebSocketにメッセージを送信
-        await self.send(text_data=json.dumps({"message": message}))
-
-
 class MatchmakingConsumer(AsyncWebsocketConsumer):
     waiting_players = []  # クラス変数として待機プレイヤーを管理
 
